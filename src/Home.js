b2596fa6ec09a0bc74b1a43838ccb5c69148d914
mart3318 <12470793+mart3318@users.noreply.github.com>
2023-03-17 16:48:09 -0500

    tutorial: Finish tutorial video #7

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,26 @@
+import { useState} from 'react';
+
 const Home = () => {
+   
+   // let name = "mario";
+    const [name, setName] = useState("mario");
+    const [age, setAge] = useState("25"); 
+
+    const handleClick = () => {
+        setName("luigi")
+        setAge("12")
+    } 
+
+    const handleClickAgain = (name) => {
+        console.log('hello' + name);
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name} is {age} years old</p>
+            <button onClick={handleClick}>Click</button>
+            <button onClick={(e) => handleClickAgain("mario", e)}>Click me again</button>
         </div>
      );
 }
