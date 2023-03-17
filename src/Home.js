6b329f3506cf90d4c37245706b0303b35a663792
mart3318 <12470793+mart3318@users.noreply.github.com>
2023-03-17 17:13:12 -0500

    tutorial: Finish tutorial video #10

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,16 @@ import { useState} from 'react';
 const Home = () => {
    
    // let name = "mario";
-    const [name, setName] = useState("mario");
+   
+   const [blogs, setBlogs] = useState ([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1},
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2},
+    { title: "Web dev top tips", body: "lorem ipsum...", author: "mario", id: 3}
+
+   ])
+
+   
+   const [name, setName] = useState("mario");
     const [age, setAge] = useState("25"); 
 
     const handleClick = () => {
@@ -17,6 +26,14 @@ const Home = () => {
 
     return ( 
         <div className="home">
+            {blogs.map((blog) => ( 
+                <div className="blog-preview" key={blog.id}>
+                   <h2>{ blog.title }</h2>
+                   <p> Written by { blog.author } </p>
+                    </div>
+
+            ))}
+
             <h2>Homepage</h2>
             <p>{ name} is {age} years old</p>
             <button onClick={handleClick}>Click</button>
